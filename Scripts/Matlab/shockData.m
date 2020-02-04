3931332f2f80508ac7dafde0bb09c858126e37d4
mliza <mliza@email.arizona.edu>
2020-02-04 13:35:14 -0700

    updated shockData

--- Scripts/Matlab/shockData.m
+++ Scripts/Matlab/shockData.m
@@ -4,16 +4,17 @@
 desktopPath = fullfile(getenv('HOME'), 'Desktop'); 
 transferPath = fullfile(desktopPath, 'Transfer'); 
 
-% Check if transfer directory exist and if not make one
+% Check if Transfer directory exist and if not make one
 if ~exist(transferPath, 'dir') 
-    mkdir(transferPath) 
+    mkdir(transferPath); 
 end 
 
-% Transfer Data from Grid to Local PC
+% Transfer data from the Grid to local computer  
 system('scp -r mliza@filexfer.hpc.arizona.edu:/home/u22/mliza/Transfer/* $HOME/Desktop/Transfer');
 
 % Add path to all subfolders 
-%addpath(genpath(
+addpath(genpath(transferPath));
 
+%detectImportOptions('Tresult-ener.dat')
 
 %end 
